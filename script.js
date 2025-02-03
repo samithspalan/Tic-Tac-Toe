9de67aace40e0f1f1c62ded963146d4894030bc2
samithspalan <samithpoojary12345@gmail.com>
2025-02-03 23:00:18 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let box=document.querySelectorAll(".box");
 let turnX=true;
+let winnerCondition=[
+    [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
+]
 box.forEach(boxes => {
     boxes.addEventListener('click',()=>{
         if(turnX){
-        boxes.innerText="X"
+        boxes.innerText="X";
+        boxes.style.color="rgb(174,51,96)"
         turnX=false;
 
         }else{
@@ -11,7 +15,7 @@ box.forEach(boxes => {
         turnX=true;
 
         }
-    }
-    )
+        
+    })
     
 })
