25e1f55994726f8140cda48c778180f79339eb8b
samithspalan <samithpoojary12345@gmail.com>
2025-02-03 21:56:36 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let box=document.querySelectorAll(".box");
+let turnX=true;
 box.forEach(boxes => {
     boxes.addEventListener('click',()=>{
+        if(turnX){
         boxes.innerText="X"
+        turnX=false;
+
+        }else{
+        boxes.innerText="O"
+        turnX=true;
+
+        }
     }
     )
     
