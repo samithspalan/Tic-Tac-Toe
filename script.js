10fc7c3d2526400c15484a33ee221e18725c8054
samithspalan <samithpoojary12345@gmail.com>
2025-02-04 11:20:46 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let box=document.querySelectorAll(".box");
 let turnX=true;
-let winnerCondition=[
-    [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
-]
+let winnerCondition = [
+    [0,1,2], [3,4,5], [6,7,8], // Rows
+    [0,3,6], [1,4,7], [2,5,8], // Columns
+    [0,4,8], [2,4,6]           // Diagonals
+];
+
 box.forEach(boxes => {
     boxes.addEventListener('click',()=>{
         if(turnX){
@@ -15,7 +18,19 @@ box.forEach(boxes => {
         turnX=true;
 
         }
-        
+        checkWinner();
     })
     
 })
+function checkWinner(){
+    for(let condition of winnerCondition){
+        let box1=box[condition[0]].innerText;
+        let box2=box[condition[1]].innerText;
+        let box3=box[condition[2]].innerText;
+        if(box1!=="" && box2!=="" && box3!=="" ){
+            if(box1===box2 && box2===box3){
+                console.log("winner");
+            }
+        }
+    }
+}
